0b62c40babf81f47a44f55eb0b69a53b601ec530
JJ Bastida <jj@athlon.studio>
2024-05-06 13:31:08 -0400

    Update the lazy loader with some new logic!

--- src/lottieLazyLoading.js
+++ src/lottieLazyLoading.js
@@ -1,10 +1,12 @@
 const lazyLotties = [...document.querySelectorAll('[data-loading="lazy"]')];
+const checkLoaded = (lottieEl) => lottieEl.children.length && (lottieEl.children[0].tagName.toLowerCase() === 'svg' || lottieEl.children[0].tagName.toLowerCase() === 'canvas');
 
 if (!!lazyLotties.length) {
   lazyLotties.forEach((lazyLottie) => {
     lazyLottie.dataset.defaultSrc = lazyLottie.dataset.src;
     lazyLottie.dataset.src = '';
     lazyLottie.dataset.animationType = 'lazy-lottie';
+    lazyLottie.setAttribute('looping', lazyLottie.dataset.loop);
     lazyLottie.style.overflow = 'hidden';
     
     if(lazyLottie.dataset.poster) {
@@ -17,34 +19,44 @@ if (!!lazyLotties.length) {
       }
     });
 
-    const lazyLottieObserver = new IntersectionObserver((entries) => entries.forEach(async (entry) => {
+    const lazyLottieLoader = new IntersectionObserver((entries) => entries.forEach(async (entry) => {
       const lottieEl = entry.target;
-      const isLoaded = lottieEl.children.length && (lottieEl.children[0].tagName === 'SVG' || lottieEl.children[0].tagName === 'CANVAS');
-      
-      if (entry.isIntersecting && lottieEl && lottieEl.style.display !== 'none') {
-        lottieEl.dataset.state = 'playing';
-        
-        if (!isLoaded) {
+ 
+      if (entry.isIntersecting && !checkLoaded(lottieEl) && lottieEl.style.display !== 'none') {  
           lottieEl.dataset.animationType = 'lottie';
           lottieEl.dataset.src = lottieEl.dataset.defaultSrc; 
-          await Webflow.require('lottie').createInstance(lottieEl);
-          lottieEl.querySelector('img') && lottieEl.querySelector('img').remove();
-          
-          if (lottieEl.dataset.playOnce === 'true') {
-            lazyLottieObserver.unobserve(lottieEl);
-          } 
-        } else {
-          lottieEl.dataset.duration = 0;
-          lottieEl.dataset.loop = 1;
-          Webflow.require('lottie').createInstance(lottieEl);
-        }
-      } else if (isLoaded) { 
           lottieEl.dataset.state = 'paused';
           lottieEl.dataset.duration = 0.01
-          lottieEl.dataset.loop = 0;
-          Webflow.require('lottie').createInstance(lottieEl);
-      }
-    }), { root: null, rootMargin: `${window.innerHeight/4}px` });
+          lottieEl.dataset.loop = lottieEl.dataset.looping;
+          await Webflow.require('lottie').createInstance(lottieEl);
+          lottieEl.querySelector('img') && lottieEl.querySelector('img').remove();
+          lazyLottieLoader.unobserve(lottieEl);
+        } 
+    }), { root: null, rootMargin: '1250px' });
   
-    lazyLotties.forEach((lottieElement) => lazyLottieObserver.observe(lottieElement));
+    const lazyLottiePlayer = new IntersectionObserver((entries) => entries.forEach((entry) => {
+      const lottieEl = entry.target;
+      
+      if(checkLoaded(lottieEl) && lottieEl.style.display !== 'none') {
+         if (entry.isIntersecting) {
+                console.log('playing')
+                lottieEl.dataset.state = 'playing';
+                lottieEl.dataset.duration = 0;
+                lottieEl.dataset.loop = lottieEl.dataset.looping;
+                Webflow.require('lottie').createInstance(lottieEl);
+
+                if (lottieEl.dataset.playOnce === 'true') {
+                  lazyLottiePlayer.unobserve(lottieEl);
+                } 
+             } else {
+                console.log('paused')
+                  lottieEl.dataset.duration = 0.01
+                  lottieEl.dataset.loop = 0;
+                  Webflow.require('lottie').createInstance(lottieEl); 
+             }
+      }
+    }), { root: null });
+  
+    lazyLotties.forEach((lottieElement) => lazyLottieLoader.observe(lottieElement));
+    lazyLotties.forEach((lottieElement) => lazyLottiePlayer.observe(lottieElement));
 }
